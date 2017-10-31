b8daa45cd5ae138e803313562f93521aaa04e935
Ire Aderinokun <i.aderinokun@eyeo.com>
2017-10-31 14:59:35 +0100

    Noissue - Add no-js/js class to HTML element

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+(function()
+{
+  document.addEventListener("DOMContentLoaded", function()
+  {
+
+    /**************************************************************************
+     * General
+     **************************************************************************/
+
+    // Change html class name from "no-js" to "js"
+    document.documentElement.className = "js";
+ 
+  }, false);
+}());
