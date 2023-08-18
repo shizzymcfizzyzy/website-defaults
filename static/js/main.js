22958cd2bbbdc71e2a1b6afa4512eee23351cb52
Dndjririnsndje Dorman <shy259565@gmail.com>
2023-08-18 22:41:26 +0000

    fhgcv

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,1 @@
-(function()
-{
-  document.addEventListener("DOMContentLoaded", function()
-  {
-
-    /**************************************************************************
-     * General
-     **************************************************************************/
-
-    // Change html class name from "no-js" to "js"
-    document.documentElement.className = "js";
- 
-  }, false);
-}());
+void
